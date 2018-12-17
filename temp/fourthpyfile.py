db258e063da50bfe3dd997960595ca32a2eecedb
vernon sims <44170825+kingvernon1@users.noreply.github.com>
2018-12-17 13:12:45 -0500

    Add files via upload

---
+++ temp/fourthpyfile.py
@@ -0,0 +1,85 @@
+# create a variable that holds 100k dice rolls in a two-dimensional array (100k rows, 2 columns) as part of the assignment
+
+# Collecting Input
+
+# import random
+
+
+# x = input("My name: ")
+
+# rolls = []
+# for n in range(10):
+# 	rolls.append([random.randint(1, 10), random.randint(1, 10)])
+
+# print("Hello,", x, ". You have the following rolls: ", rolls)
+
+
+
+# Rock Paper Scissors, but computer always picks Rock.
+
+# print ( "1) Rock\n2) Scissors\n3) Paper ")
+
+# user_input = int(input("Select a choice:"))
+# print ( type(user_input) )
+
+# if user_input == 1:
+# 	print ( "Draw!")
+# elif user_input == 2:
+# 	print ( "You lose!")
+# elif user_input == 3:
+# 	print ( "You win!")
+# else:
+# 	print ( "You didn't give a proper value")
+
+
+# ---------------------------- #
+# Count the amount of files you have in a folder, in your program
+
+
+# import os, os.path
+
+# onlyfiles = next(os.walk("/"))[2] #dir is your directory path as string
+# print(len(onlyfiles))
+
+# print(len([name for name in os.listdir('.') if os.path.isfile(name)]))
+
+
+# ---------------------------- #
+# play a fun FizzBuzz game where the user guesses how many FizzBuzz outputs there will be in n chances
+
+# Looks at an Integer
+# If the integer is divisible by 3, Print "Fizz"
+# If the integer is divisible by 5, Print "Buzz"
+# If the integer is divisible by both, Print "Fizz Buzz"
+
+# Check the Fizz-Buzz for each value between initial ( start ) & 0 -- Count Up
+
+'''
+choice = int(input("Where do we begin: "))
+
+start = 0
+while start <= choice:
+	if start%3 == 0 and start%5 == 0:
+		print("Fizz Buzz")
+	elif start%3 == 0:
+		print("Fizz")
+	elif start%5 == 0:
+		print("Buzz")
+	else:
+		print ( start )
+		pass
+
+	start += 1 # start = start + 1
+
+print (start)
+'''
+
+'''
+def make_incrementor(n):
+	return lambda x: x + n
+
+f = make_incrementor(42)
+print(f(0))
+
+print(f(1))
+'''
---
+++ temp/thirdpyfile.py
@@ -0,0 +1,95 @@
+
+def everyoneVote():
+	'''
+	# Let's create a file 
+	thoughts = open("reasons.txt", "r")
+	txtHolder = thoughts.read()
+	print("First section ", txtHolder)
+	thoughts.close()'''
+
+	# Let's write some stuff into it
+	# thoughts = open("reasons.txt", "w+")	
+	# thoughts.write("Add two paragraphs that you want to, supporting voting. \r\rYou can break lines using the backslash n and make paragraphs using backslash r.")
+	# thoughts.close()
+
+# 	# # Now let's add more stuff into it
+# 	thoughts = open("reasons.txt", "r+")
+# 	txtHolder = thoughts.readlines()
+# 	print("Third section ", txtHolder)
+# 	# ALWAYS CLOSE the file once you're done messing with it
+# 	thoughts.close()
+
+# everyoneVote()
+
+
+'''
+import json
+
+def participants():
+	# Let's open the file 
+	peopleList = open("participants.txt", "r")
+	theContents = json.loads(peopleList.read())
+	print("Say hello to the next pitchers ", theContents)
+
+	# ALWAYS CLOSE the file once you're done messing with it
+	peopleList.close()
+
+participants()
+'''
+
+
+# import json
+
+# # a Python object (dict):
+# x = {
+# 	"name": "John",
+# 	"age": 30,
+# 	"city": "New York"
+# }
+
+# # convert into JSON:
+# y = json.dumps(x)
+
+# # the result is a JSON string:
+# print(y)
+
+
+
+# with open('meke.txt', "w+") as teacher:
+# 	data = "I am done teaching at the end of January"
+# 	teacher.write(data)
+
+
+
+# import json
+# import requests
+
+# with open('stats.txt', 'r+') as info:
+
+# 	# info.write('{"one":1, "two":2}')
+# 	whatIsIt = info.readlines()
+# 	# whatIsIt = json.dumps(whatIsIt)
+# 	print ("The contents: ", whatIsIt)  
+
+# headers = { "temp_header":"three" }
+# response = requests.get("http://api.open-notify.org/iss-now.json", headers=headers)
+# print(response.status_code)
+
+
+'''
+import json
+
+print(json.dumps([1,2,3,{'4': 5, '6': 7}], separators=(',',':')))
+# Result: '[1,2,3,{"4":5,"6":7}]'
+
+print(json.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True))
+# Result: {"a": 0, "b": 0, "c": 0}
+
+print(json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=4, separators=(',', ': ')))
+
+# Result: 
+# {
+#    "4": 5,
+#    "6": 7
+# }
+'''
